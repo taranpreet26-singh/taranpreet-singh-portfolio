e1ffe5f4e335b16e264cf56da17e68994b28d672
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-06-16 00:09:33 +0530

    more animation added in project section

--- components/ProjectSection/ProjectFullSectionUI.tsx
+++ components/ProjectSection/ProjectFullSectionUI.tsx
@@ -1,11 +1,15 @@
-import { useScroll, useTransform, motion,  useMotionTemplate, easeInOut, AnimatePresence } from "framer-motion"
-import { JSX, ReactNode, useRef, useState } from "react"
-import { RiTailwindCssFill } from "react-icons/ri"
-import { SiNextdotjs, SiPrisma, SiTurborepo } from "react-icons/si"
+import { useScroll, useTransform, motion,  useMotionTemplate, easeInOut, AnimatePresence, useAnimate, stagger, useMotionValueEvent, useInView } from "framer-motion"
+import { JSX, ReactNode, useEffect, useRef, useState } from "react"
+import { CgYoutube } from "react-icons/cg"
+import { LuGithub } from "react-icons/lu"
+import { RiGlobalLine } from "react-icons/ri"
 
 type features = {
     title: string,
     info: string,
+    webLink?:string,
+    githubLink?:string,
+    ytLink?:string,
     content: ReactNode,
     tools:{
         logo:JSX.Element,
@@ -15,64 +19,52 @@ type features = {
 export default function ProjectFullSectionUI({ features }: { features: features }) {
 
     const ref = useRef<HTMLDivElement | null>(null)
+    const [pointerAnimator,animate] = useAnimate()
+    const [isOverImageWrapperActive,setImageOverWrapper]=useState<boolean>(false)
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ["start end", "end start"]
     })
     const [currentLogoInfoActive,setCurrentLogoInfoStatus] = useState<number | null>(null)
 
-    const logoInfoProjectArr = [
-        {
-            logo:(
-                <SiNextdotjs size={20} />
-            ),
-            content:(
-                <p className="text-sm ">
-                    Next.js
-                </p>
-            )
-        },
-        {
-            logo:(
-                <RiTailwindCssFill   size={20}/>
-            ),
-             content:(
-                <p className="text-sm ">
-                    Tailwind
-                </p>
-            )
-        },
-        {
-            logo:(
-                <SiTurborepo  size={20}/>
-            ),
-             content:(
-                <p className="text-sm ">
-                    Turborepo
-                </p>
-            )
-        },
-        {
-            logo:(
-                <SiPrisma size={20}/>
-            ),
-            content:(
-                <p className="text-sm ">
-                    Prisma
-                </p>
-            )
-        },
-    ]
+   
 
 
-    const translateY = useTransform(scrollYProgress, [0, 1], [300, -300])
+   
+
+    const startAnimation = ()=>{
+        animate(
+            "p",
+            {
+                opacity:1,
+                filter:"blur(0px)",
+                y:0
+            },
+            {
+                duration:0.5,
+                ease:"easeInOut",
+                delay:stagger(0.02)
+            }
+        )
+    }
+    const translateY = useTransform(scrollYProgress, [0, 1], [200, -300])
     const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0])
     const blur = useTransform(scrollYProgress,[0.55,1],[0,10])
     const scale = useTransform(scrollYProgress,[0.55,1],[1,0.7])
+    const isInView = useInView(ref, { amount: 0.3 });
+
+    useEffect(()=>{
+        if(isInView){
+            startAnimation()
+        }
+    },[isInView])
+
+   
     return <div
         ref={ref}
         className="grid group grid-cols-1 lg:grid-cols-2 gap-4 w-full h-full pb-20 lg:pb-40">
         <motion.div 
+        ref={pointerAnimator}
         style={{filter:useMotionTemplate`blur(${blur}px)`,scale:scale,
                 }}
         className="flex flex-col  pt-12 justify-center items-start gap-5">
@@ -80,9 +72,13 @@ export default function ProjectFullSectionUI({ features }: { features: features 
             <h1 className="text-xl lg:text-3xl font-bold">
                 {features?.title}
             </h1>
-            <h2 className=" text-sm text-justify text-wrap text-neutral-400/60">
-                {features?.info}
-            </h2>
+            <motion.h2 
+            className="flex flex-wrap text-sm text-justify text-wrap text-neutral-400/60">
+                {features?.info.split(" ").map((word,index)=>{
+                return <motion.p
+                style={{opacity:0,filter:"blur(10px)",y:10}}
+                key={index}>{word} &nbsp;</motion.p>})}
+            </motion.h2>
             <div className="flex flex-wrap gap-1 pl-0 lg:pl-3">
             {
                 features.tools.map((element,index)=>{
@@ -125,8 +121,33 @@ export default function ProjectFullSectionUI({ features }: { features: features 
 
         <motion.div
             style={{ y: translateY, opacity: opacity }}
-            className="h-fit pt-30  relative ">
+            className="h-fit  mt-30  relative " onMouseEnter={()=>{setImageOverWrapper(true)}} onMouseLeave={()=>{setImageOverWrapper(false)}}>
             {features?.content}
+            {
+                isOverImageWrapperActive &&
+            <div className="absolute flex items-center justify-center w-full h-full bg-neutral-400/30 rounded-xl inset-0 ">
+                
+                {
+                    features.githubLink &&
+                <div className="bg-neutral-900  border-[1px] border-neutral-400/40 p-2  rounded-full">
+                    <a  href={`${features?.githubLink}`}><LuGithub  size={20}/></a>
+                </div>
+                }
+                {
+                    features.webLink &&
+                <div className="bg-neutral-900  border-[1px] border-neutral-400/40 p-2  rounded-full">
+                    <a href={`${features?.webLink}`}><RiGlobalLine size={20} /></a>
+                </div>
+                }
+                {
+                    features.ytLink &&
+                <div className="bg-neutral-900  border-[1px] border-neutral-400/40 p-2  rounded-full">
+                    <a href={`${features?.ytLink}`}><CgYoutube  size={20}/></a>
+                </div>
+                }
+
+            </div>
+            }
             <span className="absolute  mx-auto bottom-0 w-3/4 h-px inset-x-0 bg-gradient-to-r  to-transparent via-white from-transparent"></span>
             <span className="absolute group-hover:opacity-100 opacity-0 mx-auto bottom-0 w-full h-[6px] duration-700 ease-in-out blur-sm inset-x-0 bg-gradient-to-r  to-transparent via-white from-transparent"></span>
 
