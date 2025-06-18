077a393ce5228432bfbe2427e7372d20f31789e3
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-06-19 01:18:16 +0530

    fix issue

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,6 +2,6 @@
 export default function Experience(){
     return <div className="mt-8">
         <h1 className="text-xl lg:text-3xl font-extrabold">Experience</h1>
-        
+        <p>Currently Building But you can see my project ,it's live</p>
     </div>
 }
